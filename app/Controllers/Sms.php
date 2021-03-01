5bca3a106319d8b62929ade949e34f1fea3939da
rlucasfm <richardlucasfm@gmail.com>
2021-03-01 12:06:51 -0300

    Recepção de POST

--- app/Controllers/Sms.php
+++ app/Controllers/Sms.php
@@ -2,21 +2,40 @@
 
 namespace App\Controllers;
 
+use App\Models\SmsModel;
+
 class Sms extends BaseController
 {
+    /**
+     * 
+     * Recebe o POST de informações do CRM e direciona ao Modelo
+     * Detecta e responde requisições, garantindo que apenas
+     * POST será levado ao modelo.
+     * 
+     * @var void
+     */
 	public function index()
 	{
 		if($this->request->getMethod(TRUE) !== "POST")
         {
-            echo "Este endpoint trata apenas requisições HTTP POST";
+            return $this->response->setStatusCode(405)
+                                ->setBody('Apenas método POST é permitido');
         }
         else
         {
-            $post_data = $this->request->getPost();
+            $sms = new SmsModel();
 
-            echo "<pre>";
-            var_dump($post_data);
-            echo "</pre>";
+            $post_data = $this->request->getPost();
+            try {
+                $modelResponse = $sms->salvarRelatorio($post_data);    
+            } catch (\Exception $err) {
+                $errMsg = $err->getMessage();
+                $this->response->setStatusCode(500)
+                            ->setBody("Houve um erro interno: $errMsg");
+            }            
+
+            $this->response->setStatusCode(200)
+                        ->setBody("Operação de envio de SMS enviado com sucesso. \n $modelResponse");
         }
 	}
 }
--- app/Models/SmsModel.php
+++ app/Models/SmsModel.php
@@ -4,12 +4,37 @@ use CodeIgniter\Model;
 
 class SmsModel extends Model
 {
-    protected $table      = 'sms';
-    protected $primaryKey = 'id';
-    protected $returnType = 'object';
-    protected $useSoftDeletes = false;
-    protected $allowedFields = ['id', 'celular', 'mensagem', 'clienteid', 'idsms'];
+    protected $table            = 'sms';
+    protected $primaryKey       = 'id';
+    protected $returnType       = 'object';
+    protected $useSoftDeletes   = false;
+    protected $allowedFields    = ['id', 'celular', 'mensagem', 'clienteid', 'idsms'];
     protected $useTimestamps    = false;
     protected $skipValidation   = true;
 
+    /**
+     * Identifica a operadora do SMS e trata da maneira
+     * correta, salvando a informação no banco de dados.
+     * 
+     * @var string
+     */
+    public function salvarRelatorio($data_arr)
+    {
+        
+        switch ($data_arr['idOp']) {
+            case '1':
+                // BestVoice
+                break;
+            
+            case '2':
+                // Zenvia
+                break;
+
+            default:
+                $result = "Operadora não reconhecida";
+                break;
+        }
+
+        return $result;
+    }
 }
